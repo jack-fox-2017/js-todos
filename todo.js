6f358e5c9e6fb0487317fd4ecfc0ba44697a2884
Rahmat H <kickyou44@gmail.com>
2017-08-02 15:17:37 +0700

    finished

--- todo.js
+++ todo.js
@@ -11,69 +11,73 @@ class ToDo {
 		switch(arr[0]) {
 			case undefined:
 			case 'help':
-				let result = [
-					'$ node todo.js',
-					'$ node todo.js help',
-					'$ node todo.js list',
-					'$ node todo.js add <task_content>',
-					'$ node todo.js task <task_id>',
-					'$ node todo.js delete <task_id>',
-					'$ node todo.js complete <task_id>',
-					'$ node todo.js uncomplete <task_id>'
-				]
-				console.log(result.join('\n'))
+				console.log(ViewToDo.showHelp());
 				break;
 
 			case 'list':
-				console.log(data.map(item => {return `${item.id}. [${item.completed ? 'x' : ' '}] ${item.content}`}).join('\n'));
+				console.log(ViewToDo.showList(data));
 				break;
 
 			case 'add':
-
 				if (content == undefined) {
-					console.log('please write a content');
+					console.log(ViewToDo.needParam());
 					break;
 				}
 
 				let newToDo = {
 					id: data.length + 1,
 					completed: false,
-					content: content
+					content: content,
+					completedDate: null,
+					createdDate: new Date(),
+					tags: []
 				}
 
 				data.push(newToDo)
 				ModelToDo.save(file, data)
 
-				console.log(`Added "${content}" to your TODO list...`);
+				console.log(ViewToDo.success('Added', content));
 				break;
 
 			case 'task':
+				if (taskId == undefined) {
+					console.log(ViewToDo.needParam());
+					break;
+				}
+
 				let taskFound = data.filter(item => {return item.id == taskId})
 				if (taskFound.length <= 0) 
-					console.log(`Can't find todo with id ${taskId}`);
+					console.log(ViewToDo.notFound(taskId));
 				else
-					console.log(`${taskFound[0].id}. [${taskFound[0].completed ? 'x' : ' '}] ${taskFound[0].content}`);
+					console.log(ViewToDo.showSingleList(taskFound[0]));
 				break;
 
 			case 'delete':
+				if (taskId == undefined) {
+					console.log(ViewToDo.needParam());
+					break;
+				}
+
 				let deleteFound = data.filter(item => {return item.id == taskId})
 				if (deleteFound.length <= 0)
-				 	console.log(`Can't delete todo with id ${taskId}, not found`);
+				 	console.log(ViewToDo.notFound(taskId));
 				else {
 					let afterDeleted = data.filter(item => {return item.id != taskId})
 					for (let i = 0; i < afterDeleted.length; i++) {
 						afterDeleted[i].id = i + 1
 					}
 					ModelToDo.save(file, afterDeleted)
-					console.log(`Deleted "${deleteFound[0].content}" to your TODO list...`);
+					console.log(ViewToDo.success('Deleted', deleteFound[0].content));
 				}
 				break;
 
 			case 'complete':
 				for (let i = 0; i < data.length; i++) {
 					if (data[i].id == taskId) {
+						data[i].completeDate = new Date()
 						data[i].completed = true
 						ModelToDo.save(file, data)
+						console.log(ViewToDo.success('Marked', data[i].content));
 						break;
 					}
 				}
@@ -82,15 +86,108 @@ class ToDo {
 			case 'uncomplete':
 				for (let i = 0; i < data.length; i++) {
 					if (data[i].id == taskId) {
+						data[i].completeDate = null
 						data[i].completed = false
 						ModelToDo.save(file, data)
+						console.log(ViewToDo.success('Unmarked', data[i].content));
 						break;
 					}
 				}
 				break;
-			
+
+			case 'tag':
+				if (taskId == undefined) {
+					console.log(ViewToDo.needParam());
+					break;
+				}
+
+				for (let i = 0; i < data.length; i++) {
+					if (data[i].id == taskId) {
+						let tags = command.slice(2)
+						data[i].tags = data[i].tags.concat(tags)
+						ModelToDo.save(file, data)
+						console.log(ViewToDo.success('Tagged', data[i].content, `with tags: ${tags.join(', ')}`));
+						break;
+					}
+				}
+
+				break;
+
 			default:
-				
+				let split = command[0].split(':')
+				switch(split[0]) {
+
+					case 'list':
+						switch(split[1]) {
+
+							case 'created':
+								let orderList = command[1]
+
+								switch(orderList) {
+									case undefined:
+									case 'asc':
+										console.log(ViewToDo.showListOrdered(data.sort((a,b) => {return a.createdDate > b.createdDate})));
+										break;
+									case 'desc':
+										console.log(ViewToDo.showListOrdered(data.sort((a,b) => {return a.createdDate < b.createdDate})));
+										break;
+									default:
+										console.log(ViewToDo.showCommandNotFound());
+								}
+							
+								break;
+
+							case 'completed':
+								let completeList = command[1]
+
+								switch(completeList) {
+									case undefined:
+									case 'asc':
+										console.log(ViewToDo.showListOrdered(data.filter(item => {return item.completed == true}).sort((a,b) => {return a.completeDate > b.completeDate})));
+										break;
+									case 'desc':
+										console.log(ViewToDo.showListOrdered(data.filter(item => {return item.completed == true}).sort((a,b) => {return a.completeDate < b.completeDate})));
+										break;
+									default:
+										console.log(ViewToDo.showCommandNotFound());
+								}
+								break;
+
+								case 'outstanding':
+								let outstandingList = command[1]
+								
+								switch(outstandingList) {
+									case undefined:
+									case 'asc':
+										console.log(ViewToDo.showListOrdered(data.filter(item => {return item.completed == false}).sort((a,b) => {return a.createdDate > b.createdDate})));
+										break;
+									case 'desc':
+										console.log(ViewToDo.showListOrdered(data.filter(item => {return item.completed == false}).sort((a,b) => {return a.createdDate < b.createdDate})));
+										break;
+									default:
+										console.log(ViewToDo.showCommandNotFound());
+								}
+								break;
+
+							default:
+								console.log(ViewToDo.showCommandNotFound());
+						}
+						break;
+
+					case 'filter':
+						if (split[1] == undefined) {
+							console.log(ViewToDo.needParam());
+							break;
+						}
+
+						let filtered = data.filter(item => {return item.tags.indexOf(split[1]) >= 0})
+						console.log(ViewToDo.showList(filtered));
+
+						break;
+
+					default:
+						console.log(ViewToDo.showCommandNotFound());
+				}
 		}
 	}
 }
--- view.js
+++ view.js
@@ -1,5 +1,54 @@
 class ViewToDo {
-	
+	static showHelp() {
+		let result = [
+			'Available command :',
+			'$ node todo.js',
+			'$ node todo.js help',
+			'$ node todo.js list',
+			'$ node todo.js add <task_content>',
+			'$ node todo.js task <task_id>',
+			'$ node todo.js delete <task_id>',
+			'$ node todo.js complete <task_id>',
+			'$ node todo.js uncomplete <task_id>',
+			'$ node todo.js list:created asc|desc',
+			'$ node todo.js list:completed asc|desc',
+			'$ node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>',
+			'$ node todo.js filter:<tag_name>',
+		]
+		return result.join('\n')
+	}
+
+	static showCommandNotFound() {
+		return `Command not found!\n${this.showHelp()}`
+	}
+
+	static showSingleList(data, order) {
+		return `${order || data.id}. [${data.completed ? 'x' : ' '}] ${data.content}`
+	}
+
+	static showList(data) {
+		return data.map(item => {
+			return this.showSingleList(item)
+		}).join('\n')
+	}
+
+	static showListOrdered(data) {
+		return data.map((item, index) => {
+			return this.showSingleList(item, index + 1)
+		}).join('\n')
+	}
+
+	static needParam() {
+		return `Command need more params\n${this.showHelp()}`
+	}
+
+	static success(job, content, information) {
+		return `${job} "${content}" ${information || 'to your TODO list...'}`
+	}
+
+	static notFound(id) {
+		return `Can't found list with id ${id}`
+	}
 }
 
 module.exports = ViewToDo
