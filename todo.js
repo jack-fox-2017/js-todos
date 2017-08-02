e62c425064463328d28045e27e15f423f23c4e9c
Rahmat H <kickyou44@gmail.com>
2017-08-02 10:17:57 +0700

    release 0

--- todo.js
+++ todo.js
@@ -1,1 +1,46 @@
+class ToDo {
+	static command(arr) {
+		switch(arr[0]) {
+			case undefined:
+			case 'help':
+				let result = [
+					'$ node todo.js',
+					'$ node todo.js help',
+					'$ node todo.js list',
+					'$ node todo.js add <task_content>',
+					'$ node todo.js task <task_id>',
+					'$ node todo.js delete <task_id>',
+					'$ node todo.js complete <task_id>',
+					'$ node todo.js uncomplete <task_id>'
+				]
+				return result.join('\n')
+			case 'list':
+				
+				break;
+			case 'add':
+				//arr[1] content
+				break;
+			case 'task':
+				//arr[1]
+				break;
+			case 'delete':
+				//arr[1]
+				break;
+			case 'complete':
+				//arr[1]
+				break;
+			case 'uncomplete':
+				//arr[1]
+				break;
+			
+			default:
+				
+		}
+	}
+}
 
+
+
+const command = process.argv.slice(2)
+// console.log(command);
+console.log(ToDo.command(command));
