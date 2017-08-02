071e30287c962fca6d1afd113241e86d9ed1fcfc
Pragistyo <new.SOS.hope@outlook.com>
2017-08-02 22:00:11 +0700

    masih release 1

---
+++ model.js
@@ -0,0 +1,69 @@
+// const Todo = require ('./todo.js')
+// const View = require ('./view.js')
+const fs  = require('fs')
+let rawdata = fs.readFileSync('data.json','utf8')
+
+class Model{
+
+  static readData(){
+    var rawdataobj= JSON.parse(rawdata)
+    return rawdataobj
+  }
+
+  static adds(){
+    let stradd = process.argv[3]
+    var obj = {}
+    var rawdataobj= JSON.parse(rawdata)
+    var length = rawdataobj.length+1
+    obj['ID']= length
+    obj['complete']= false
+    obj['created_date'] = new Date().toISOString()
+    obj['task']= stradd
+    obj['completed_date'] = null
+    rawdataobj.push(obj)
+    var writefile = JSON.stringify(rawdataobj)
+    fs.writeFileSync('data.json',writefile,'utf8')
+    return rawdataobj
+    // return writefile
+  }
+
+  static deletes(){
+    var rawdataobj= JSON.parse(rawdata)
+    var input = process.argv
+      var index = process.argv[3]-1
+      var splice = rawdataobj.splice(index,1)
+      var writefile = JSON.stringify(rawdataobj)
+      fs.writeFileSync('data.json',writefile,'utf8')
+      return rawdataobj
+  }
+
+  static completes(){
+    var rawdataobj= JSON.parse(rawdata)
+    var index = process.argv[3]-1
+    rawdataobj[index].complete = true
+    rawdataobj[index].completed_date = new Date().toISOString()
+    var writefile = JSON.stringify(rawdataobj)
+    fs.writeFileSync('data.json',writefile,'utf8')
+    return rawdataobj
+    // return rawdataobj[index].complete
+  }
+
+  static uncompletes(){
+    var rawdataobj= JSON.parse(rawdata)
+    var index = process.argv[3]-1
+    rawdataobj[index].complete = false
+    var writefile = JSON.stringify(rawdataobj)
+    fs.writeFileSync('data.json',writefile,'utf8')
+    return rawdataobj
+  }
+
+  static list_created(){
+
+  }
+
+}
+
+// console.log(Model.readData());
+
+
+module.exports = Model
---
+++ view.js
@@ -0,0 +1,36 @@
+const fs  = require('fs')
+// const Todo = require('./todo.js')
+// const Controller = require ('./todo.js')
+const Model = require('./model');
+
+class View {
+
+  static list(data){
+    // let data = Model.readData()
+    let tmp =[];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].complete == true){
+      tmp.push(i+1 +'. '+'[V] '+ `${data[i].task}`)
+      }
+      else {
+      tmp.push(i+1 +'. '+'[ ] '+ `${data[i].task}`)
+      }
+    }
+    return tmp.join('\n')
+  }
+
+  static task(){
+    var arr = View.list(Model.readData()).split('\n')
+    let id = process.argv[3]
+    return arr[id-1]
+  }
+
+  static delete(){
+
+  }
+
+}
+
+
+// console.log(View.list());
+module.exports = View
