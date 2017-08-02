b40ac742ff9b7355f4a31f3b3b945a843ec53d54
alfandash <cumiasem91@gmail.com>
2017-08-02 17:47:00 +0700

    tag done

--- model.js
+++ model.js
@@ -1,15 +1,13 @@
 const fs = require('fs')
 
-class Read {
+class Model {
   static readData(file){
     let data = fs.readFileSync(file,'utf-8');
     let dataJSON = JSON.parse(data);
     return dataJSON
   }
   static writeData(file,data){
-    let dataJSON = JSON.stringify(data);
-    //console.log(dataJSON);
-    //console.log(data);
+    let dataJSON = JSON.stringify(data, null, ' ');
     fs.writeFile(file,dataJSON), function(err) {
       if(err) throw err;
       return console.log('data.json berhasil di update');
@@ -17,4 +15,4 @@ class Read {
   }
 }
 
-module.exports = Read;
+module.exports = Model;
--- todo.js
+++ todo.js
@@ -9,13 +9,21 @@ const Model = require(`./model.js`)
 const View = require(`./view.js`)
 
 let temp = process.argv
-let input = temp.slice(2)
+let inputCLI = temp.slice(2)
 let file = ''
-console.log(input);
+console.log(inputCLI);
 
-function manageInput(input){
+class Tag{
+  constructor(tag){
+    this._tag = tag
+  }
+}
+
+function manageInput(inputCLI){
+  // split jika terdapat :
+  let input = inputCLI.map(function(x){return x.split(':')})
   //console.log(input[0]);
-  switch (input[0]) {
+  switch (input[0][0]) {
     case `help`: {
       let dataJSON = Model.readData('./dataList.json')
       View.showListCommand(dataJSON)
@@ -23,35 +31,120 @@ function manageInput(input){
 
     case `list`: {
       let data = Model.readData('./data.json')
-      View.showListTask(data)
+      //console.log(input.length);
+      let property = ''
+      let sort = ''
+      if (input.length > 1) {
+        sort = input[1][0]
+      } else {
+        sort = `asc`
+      }
+      switch (input[0][1]) {
+        case 'created': {property=`created_at`}break;
+        case 'status': {property=`status`}break;
+        case 'task': {property=`task`}break;
+        case 'complete' : {property=`complete`}break;
+        case 'uncomplete' : {property=`uncomplete`}break;
+        default: {property=``}
+      }
+      View.showListTask(data,property,sort)
+
     }break;
 
     case `add`: {
       let data = Model.readData('./data.json')
-      let newTask = {task: input[1]}
+      let today = new Date()
+      let sentence = input[1][0].charAt(0).toUpperCase() + input[1][0].slice(1);
+      //console.log(data.length);
+      let newTask = {
+        id: data.length + 1,
+        status: 0,
+        task: sentence,
+        created_at : today.toISOString()
+      }
       data.push(newTask)
       Model.writeData('./data.json',data)
     }break;
 
     case `task`:{
       let data = Model.readData('./data.json')
-      View.showFind(data,input[1])
+      View.showFind(data,input[1][0])
     }
     break;
 
     case `delete`:{
       let data = Model.readData('./data.json')
-      View.showDelete(data,input[1])
-      let number = Number(input[1]) - 1
-      //console.log(number);
-      data.splice(number,1)
-      //console.log(data);
-      Model.writeData('./data.json',data)
+      let temp =[]
+      for (var i = 0; i < data.length; i++) {
+        if(Number(data[i][`id`])!==Number(input[1][0])){
+          temp.push(data[i])
+        }
+      }
+      Model.writeData('./data.json',temp)
     }
     break;
 
-    default:
+    case `complete`:{
+      let data = Model.readData('./data.json')
+      let temp = data.map(function(x){
+        if(x.id === Number(input[1][0])){
+          x.status = 1
+        }
+        return x
+      })
+      Model.writeData('./data.json',temp)
+    }
+    break;
+
+    case `uncomplete`:{
+      let data = Model.readData('./data.json')
+      let temp = data.map(function(x){
+        if(x.id === Number(input[1][0])){
+          x.status = 0
+        }
+        return x
+      })
+      Model.writeData('./data.json',temp)
+    }
+    break;
+
+    case `filter`:{
+
+    }
+
+    case `tag`: {
+      let tag = input.splice(2)
+      let data = Model.readData('./data.json')
+      let temp = data.map(function(x){
+        if(x.id === Number(input[1][0])){
+          let obj=[]
+          for (let i = 0; i < tag.length; i++) {
+            obj.push(new Tag(tag[i]))
+          }
+          x.tag=obj
+          return x
+        } else {
+          return x
+        }
+      })
+      let task = data.filter(function(x){
+        if(x.id === Number(input[1][0])){
+          return true
+        } else {
+          return false
+        }
+      })
+      // write data
+      Model.writeData('./data.json',temp)
+      // show log
+      var word = `Tagged task "${task[0][`task`]}" with tags: ${tag.join(', ')}`
+      View.showTag(word)
+    }break;
+
+    default:{
+      View.showError()
+    }
   }
 }
 
-manageInput(input)
+manageInput(inputCLI)
